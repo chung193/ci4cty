e4e68666bf126924f70e4c96159a0fda932577c0
chung193 <chungvd.it@gmail.com>
2022-12-01 11:32:38 +0700

    backend

---
+++ public/backend/js/validate.js
@@ -0,0 +1,132 @@
+// all validate here
+$(document).ready(function () {
+  $("#addCategory").validate({
+    onsubmit: true,
+    rules: {
+      "title": {
+        required: true,
+      }
+    },
+    messages: {
+      "title": {
+        required: "Trường này là bắt buộc",
+      },
+    }
+  });
+
+  $("#editCategory").validate({
+    onsubmit: true,
+    rules: {
+      "title": {
+        required: true,
+      }
+    },
+    messages: {
+      "title": {
+        required: "Trường này là bắt buộc",
+      },
+    }
+  });
+
+
+  $("#addPost").validate({
+    onsubmit: true,
+    rules: {
+      "title": {
+        required: true,
+      },
+      "content": {
+        required: true,
+      }
+    },
+    messages: {
+      "title": {
+        required: "Trường này là bắt buộc",
+      },
+      "content": {
+        required: "Trường này là bắt buộc",
+      },
+    }
+  });
+
+  $("#editPost").validate({
+    onsubmit: true,
+    rules: {
+      "title": {
+        required: true,
+      },
+      "content": {
+        required: true,
+      }
+    },
+    messages: {
+      "title": {
+        required: "Trường này là bắt buộc",
+      },
+      "content": {
+        required: "Trường này là bắt buộc",
+      },
+    }
+  });
+
+  $("#addPage").validate({
+    onsubmit: true,
+    rules: {
+      "title": {
+        required: true,
+      },
+      "content": {
+        required: true,
+      }
+    },
+    messages: {
+      "title": {
+        required: "Trường này là bắt buộc",
+      },
+      "content": {
+        required: "Trường này là bắt buộc",
+      },
+    }
+  });
+
+  $("#editPage").validate({
+    onsubmit: true,
+    rules: {
+      "title": {
+        required: true,
+      },
+      "content": {
+        required: true,
+      }
+    },
+    messages: {
+      "title": {
+        required: "Trường này là bắt buộc",
+      },
+      "content": {
+        required: "Trường này là bắt buộc",
+      },
+    }
+  });
+
+  $("#editSeo").validate({
+    onsubmit: true,
+    rules: {
+      "meta_title": {
+        required: true,
+      },
+      "meta_description": {
+        required: true,
+      },
+    },
+    messages: {
+      "meta_title": {
+        required: "Trường này là bắt buộc",
+      },
+      "meta_description": {
+        required: "Trường này là bắt buộc",
+      },
+    }
+  });
+
+});
